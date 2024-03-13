ef0597f884a46695074b4525b7ea837e48d5ea7f
Jordan Romagnoli <146195147+JordanRomagnoli@users.noreply.github.com>
2024-03-13 15:22:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script>
+
+/*
+    Importazione componente
+*/
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import Axios from 'axios';
+import { store } from './store';
+
+export default {
+    data() {
+        return { 
+            
+            store
+            
+        }
+    },
+    /*
+        Dichiarazione dei componenti all'interno dell'oggetto
+    */
+    components: {
+
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+}
+</script>
+
+<template>
+    <div>
+        
+        <header>
+            <AppHeader/>
+        </header>
+        <main>
+            <AppMain/>
+        </main>
+        <footer>
+            <AppFooter/>
+        </footer>
+        
+    </div>
+</template>
+
+<style lang="scss">
+    @import "bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data() {
+            return { 
+                 
+            }
+        },
+    }
+    </script>
+
+<template>
+    <main class="main-title">
+        
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @use '../assets/scss/footer.scss' as *;
+</style>
